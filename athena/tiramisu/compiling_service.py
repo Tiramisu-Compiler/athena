5b01fdd16e3e026ca4b12133c8b13c73223388bf
Smail KOURTA <sk10691@nyu.edu>
2023-07-31 13:16:22 +0400

    typo fix

--- athena/tiramisu/compiling_service.py
+++ athena/tiramisu/compiling_service.py
@@ -121,7 +121,7 @@ class CompilingService:
 
     fct->gen_time_space_domain();
     fct->gen_isl_ast();
-    fct->print_asl_ast_representation(nullptr, 0);
+    fct->print_isl_ast_representation(nullptr, 0);
 """
 
         # Paste the lines responsable of checking legality of schedule in the cpp file
@@ -188,7 +188,7 @@ class CompilingService:
 
     fct->gen_time_space_domain();
     fct->gen_isl_ast();
-    fct->print_asl_ast_representation(nullptr, 0);
+    fct->print_isl_ast_representation(nullptr, 0);
 """
 
         # Paste the lines responsable of generating the program json tree in the cpp file
